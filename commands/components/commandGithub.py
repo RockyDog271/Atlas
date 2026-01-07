5a89de8913cb7f3e81cc24a73584a82bec4eb7f3
Logan Simila <142363233+RockyDog271@users.noreply.github.com>
2026-01-06 23:34:51 -0500

    Push before Pull

--- commands/components/commandGithub.py
+++ commands/components/commandGithub.py
@@ -1,6 +1,7 @@
 GithubLink = "https://github.com/RockyDog271/Atlas"
 
 async def commandGithub(message):
+    if len
     await message.channel.send(
         f"Here is the GitHub link:"
         f"{GithubLink}"
